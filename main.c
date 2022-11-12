4892d9502034961abe0cc80c3ddf9926dfcd5f67
jeremysid <116936076+jeremysid@users.noreply.github.com>
2022-11-12 12:33:11 +0800

    second part update
    note :
    second part not finished.

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-
+#include<string.h>
 // global variables
 	char name[30];
 	
@@ -13,18 +13,29 @@ void firstPart() {
 
 void secondPart() {
 
-	// char pick[] = ();
-
-	printf("");
 	char choices[] = {'A', ',', 'B', '\0'};
-	
+	char choiceOne[] = "Start";
+	char choiceTwo[] = "Exit";
+
 	printf("Choices: \n");
-	printf("%c", choices[0]);
+	printf("%s", choiceOne);
 	printf("%c", choices[1]);
-	printf("%c", choices[2]);
+	printf("%s\n", choiceTwo);
 
+	char userChoice[10];
 
+	scanf("%s\n", &userChoice);
 
+	int resOne = strncmp(choiceOne, userChoice, 10);
+	int resTwo = strncmp(choiceTwo, userChoice, 10);
+
+	if (resOne == 0) {
+		printf("Start");
+	}
+	
+	if (resTwo == 0) {
+		printf("Exit");
+	}
 }
 
 int main() {
