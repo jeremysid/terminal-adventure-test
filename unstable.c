a00dd7868b977478871cb415b7ceb369afb4fe3e
Espekari <116936076+jeremysid@users.noreply.github.com>
2022-12-21 14:01:14 +0800

    unstable.c update
    - now uses else ifs and else 
    - less janky

--- unstable.c
+++ unstable.c
@@ -1,19 +1,6 @@
 #include<stdio.h>
 #include<string.h>
-
-// global variables
-char name[30];
-
-// Functions
-void firstPart();
-void secondPart();
-void thirdPart();
-void monologue();
-void nothing();
-void unstablePath();
-void unstablePathChoices();
-void continuePromptStable();
-void continuePromptUnstable();
+#include"functions.h"
 
 int startFunc(void) {
 
@@ -56,10 +43,9 @@ void secondPart() {
     
     if (*input == 1) (
         thirdPart()
-    );
-    if (*input == 2) {
+    ); else if (*input == 2) {
         exitFunc();
-    } 
+    } else { exitFunc(); }
     
 }
 
@@ -74,7 +60,6 @@ void monologue() {
     char options[] = {"pick from options: Stable(1), Unstable(2)\n"};
     int stable = 1;
     int unstable = 2;
-    int nothng = 3;
 	int input[10];
 
 	printf("%s", introTwo);
@@ -83,26 +68,14 @@ void monologue() {
 	scanf("%d", input);	
 
     if (stable == *input) {
-        // printf("To Improve\n");
-        // continuePromptStable();
         printf("Not Available\n");
         exitFunc();
-    }
-    if (unstable == *input) {
+    } else if (unstable == *input) {
         printf("To be Alive? \n");
         printf("???: Interesting \n");
         continuePromptUnstable();
-    }
-    if (nothng == *input) {
-        nothing();
-    }
+    } else { exitFunc(); }
     
-}
-
-void nothing() {
-
-	printf("Why do you choose Nothing?\n");
-
 }
 
 void unstablePath() {
@@ -132,7 +105,6 @@ void continuePromptUnstable() {
 	if (yes == *input) {
 		printf("Continue\n");
 		unstablePath();
-	} 
-	if (no ==*input) ( exitFunc() );
-
+	} else if (no ==*input) ( exitFunc() );
+	  else { exitFunc(); }
 }
